c3f826baa682610724ea051a4451112b7d14a549
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2023-04-24 11:59:00 +0530

    still working on cache

--- phase 3/src/cache.cpp
+++ phase 3/src/cache.cpp
@@ -1,9 +1,37 @@
 #include "cache.hpp"
 #include "builtin_funcs.hpp"
+#include "self_defined_funcs.hpp"
+Cache::Cache(int cache_size,int block_size,int ways,int block_replacement_policy,int hit_time,int miss_time){
+    this->cache_size=cache_size;
+    this->cache_block_size=block_size;
+    this->mapping_choice=block_replacement_policy;
+    this->ways=ways;
+    this->hit_time=hit_time;
+    this->miss_time=miss_time;
+    this->index_bits=((int)log2(cache_size/cache_block_size)) - ( (int) log2(ways) );;
+    int block_offset_bits=(int)log2(cache_block_size);
+    int tag_bits=32 - this->index - this->block_offset_bits;
+}
+Cache::Cache(int cache_size,int block_size,int ways,int block_replacement_policy){
+    this->cache_size=cache_size;
+    this->cache_block_size=block_size;
+    this->mapping_choice=block_replacement_policy;
+    this->ways=ways;
+    this->hit_time=1;
+    this->miss_time=20;
+    this->index_bits=((int)log2(cache_size/cache_block_size)) - ( (int) log2(ways) );;
+    int block_offset_bits=(int)log2(cache_block_size);
+    int tag_bits=32 - this->index - this->block_offset_bits;
+}
 void Cache::createCache(){
     mycache=(struct cache_entry *)malloc(pow(2,index_bits)*sizeof(struct cache_entry));
     char * temp=(char *)malloc(pow(2,index_bits)*cache_block_size*sizeof(char));
     for(int i=0;i<pow(2,index_bits);i++){
         mycache[i].block=temp+(i*cache_block_size);
     }
+}
+
+unsigned long long int Cache::readCache(unsigned int address, int bytes){
+    str_add=dec2bin(address);
+    
 }
--- phase 3/src/cache.hpp
+++ phase 3/src/cache.hpp
@@ -13,8 +13,11 @@ class Cache{
     int hit_time;
     int miss_time;
     int index_bits;
-    int block_offset;
+    int block_offset_bits;
+    int tag_bits;
     struct cache_entry * mycache;
     void createCache();
-
+    unsigned long long int readCache(unsigned int address, int bytes);
+    Cache(int cache_size,int block_size,int ways,int block_replacement_policy,int hit_time,int miss_time);
+    Cache(int cache_size,int block_size,int ways,int block_replacement_policy);
 };
